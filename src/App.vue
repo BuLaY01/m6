80f8e4302bf8960012efc4e71043f598786f97eb
BuLaY01 <mishastar544@gmail.com>
2025-01-22 16:36:11 +0200

    Game

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+//import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+
+</script>
+
+<template>
+<main>
+  <Header/>
+</main>
+</template>
+
+<style scoped>
+
+</style>
